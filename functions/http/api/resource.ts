8974cc52395714586e9073e0697e220406bb9305
Andrew Sanchez <asanchez@yext.com>
2023-03-17 09:25:01 -0400

    add resource endpoint

---
+++ functions/http/api/resource.ts
@@ -0,0 +1,9 @@
+export default function Resource(request) {
+    const { pathParams, queryParams, site } = request
+
+    console.log('Site: ', site)
+    console.log("Path Params: ", pathParams)
+    console.log("Query Params: ", queryParams)
+
+    return { "hello": "world" }
+}
