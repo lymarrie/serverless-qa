c58b3915500b0db02e00bcf2e572f5857663f909
Andrew Sanchez <asanchez@yext.com>
2023-03-24 12:08:52 -0400

    update response

--- functions/http/resource.ts
+++ functions/http/resource.ts
@@ -4,4 +4,10 @@ export default function main(request) {
     console.log('Site: ', site)
     console.log("Path Params: ", pathParams)
     console.log("Query Params: ", queryParams)
+
+    return {
+        body: "Hello World",
+        headers: null,
+        statusCode: 200
+    }
 }
